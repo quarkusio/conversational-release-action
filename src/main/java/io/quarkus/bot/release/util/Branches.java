729cb210dbd7c657c6fdf37dfbf614b7183b734a
Guillaume Smet <guillaume.smet@gmail.com>
2024-07-08 17:13:47 +0200

    Mark 3.15 as the next LTS

--- src/main/java/io/quarkus/bot/release/util/Branches.java
+++ src/main/java/io/quarkus/bot/release/util/Branches.java
@@ -7,7 +7,7 @@ import io.quarkus.bot.release.ReleaseInformation;
 public class Branches {
 
     public static final String MAIN = "main";
-    public static final List<String> LTS_BRANCHES = List.of("3.8", "3.2", "2.13");
+    public static final List<String> LTS_BRANCHES = List.of("3.15", "3.8", "3.2", "2.13");
     public static final String BRANCH_2_13 = "2.13";
 
     public static String getPlatformPreparationBranch(ReleaseInformation releaseInformation) {
