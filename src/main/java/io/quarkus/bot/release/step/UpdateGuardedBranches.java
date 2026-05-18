9086e1be17204f1066d576c8e029aac099153d37
Guillaume Smet <guillaume.smet@gmail.com>
2026-05-18 11:03:58 +0200

    Update guarded branches for LTS releases

---
+++ src/main/java/io/quarkus/bot/release/step/UpdateGuardedBranches.java
@@ -0,0 +1,39 @@
+package io.quarkus.bot.release.step;
+
+import java.io.IOException;
+import java.util.List;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GitHub;
+
+import io.quarkiverse.githubaction.Commands;
+import io.quarkiverse.githubaction.Context;
+import io.quarkus.arc.Unremovable;
+import io.quarkus.bot.release.ReleaseInformation;
+import io.quarkus.bot.release.ReleaseStatus;
+import io.quarkus.bot.release.util.Branches;
+import io.quarkus.bot.release.util.Processes;
+import io.quarkus.bot.release.util.UpdatedIssueBody;
+
+@Singleton
+@Unremovable
+public class UpdateGuardedBranches implements StepHandler {
+
+    @Inject
+    Processes processes;
+
+    @Override
+    public boolean shouldSkip(ReleaseInformation releaseInformation, ReleaseStatus releaseStatus) {
+        return !releaseInformation.isFirstFinal() || !Branches.isLts(releaseInformation.getBranch());
+    }
+
+    @Override
+    public int run(Context context, Commands commands, GitHub quarkusBotGitHub, ReleaseInformation releaseInformation,
+            ReleaseStatus releaseStatus, GHIssue issue, UpdatedIssueBody updatedIssueBody)
+            throws IOException, InterruptedException {
+        return processes.execute(List.of("./update-guarded-branches.sh"));
+    }
+}
