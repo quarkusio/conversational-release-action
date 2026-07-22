634a149f463619635777db8b0f189a41c24b782b
Guillaume Smet <guillaume.smet@gmail.com>
2026-07-22 17:46:52 +0200

    Release main and branches >= 4.0 with Java 21

--- src/main/java/io/quarkus/bot/release/util/Jdks.java
+++ src/main/java/io/quarkus/bot/release/util/Jdks.java
@@ -12,10 +12,15 @@ public class Jdks {
 
     private static final String JDK_11 = "11";
     private static final String JDK_17 = "17";
+    private static final String JDK_21 = "21";
 
     public String getJdkVersion(String branch) {
         if (MAIN.equals(branch)) {
-            return JDK_17;
+            return JDK_21;
+        }
+
+        if (Versions.getVersion(branch).compareTo(Versions.VERSION_4_0) >= 0) {
+            return JDK_21;
         }
 
         if (Versions.getVersion(branch).compareTo(Versions.VERSION_3_7) >= 0) {
